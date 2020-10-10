94ce8be9f74edcc53f14639dcbdc07810894005c
Vadim3x4 <kurnuli2010@yandex.ru>
2020-10-10 23:33:24 +0400

    homevork_v2

--- api/urls.py
+++ api/urls.py
@@ -10,11 +10,11 @@ from .views import (PostViewSet, CommentViewSet,
 
 
 router_v1 = DefaultRouter()
-router_v1.register('posts', PostViewSet, basename='posts')
-router_v1.register(r'posts/(?P<post_id>\d+)/comments',
+router_v1.register('v1/posts', PostViewSet, basename='posts')
+router_v1.register(r'v1/posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='post_id')
-router_v1.register('group', GroupViewSet, basename='groups')
-router_v1.register('follow', FollowViewSet, basename='followers')
+router_v1.register('v1/group', GroupViewSet, basename='groups')
+router_v1.register('v1/follow', FollowViewSet, basename='followers')
 
 
 urlpatterns = router_v1.urls
